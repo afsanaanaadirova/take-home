11eb92bdad660d49cdb0a91a58e8ad6c00d0077b
afsanashn <afsanashn@code.edu.az>
2024-11-27 17:01:48 +0400

    feat(Button): add expand/collapse functionality and animations

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ListItem } from "../api/getListData";
 import { DeleteButton, ExpandButton } from "./Buttons";
 import { ChevronUpIcon } from "./icons";
@@ -6,27 +6,45 @@ import { ChevronUpIcon } from "./icons";
 type CardProps = {
   title: ListItem["title"];
   description: ListItem["description"];
+  isDeleted?: boolean;
+  onDelete?: () => void;
 };
 
-export const Card: FC<CardProps> = ({ title, description }) => {
+export const Card: FC<CardProps> = ({ title, description, isDeleted, onDelete }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="border border-black px-2 py-1.5">
       <div className="flex justify-between mb-0.5">
         <h1 className="font-medium">{title}</h1>
-         {/* <ToggleButton
+        {/* <ToggleButton
           isActive={visible}
           onToggle={() => setVisible(!visible)}
           active={<ChevronUpIcon />}
           inactive={<ChevronDownIcon/>}
         /> */}
-        <div className="flex">
-          <ExpandButton>
-            <ChevronUpIcon />
-          </ExpandButton>
-          <DeleteButton />
-        </div>
+        {!isDeleted && (
+          <div className="flex">
+            <ExpandButton
+              onClick={() => setVisible(!visible)}
+              className={`transform transition-transform duration-300 ${visible ? "rotate-180" : "rotate-0"
+                }`}
+            >
+              <ChevronUpIcon />
+            </ExpandButton>
+            <DeleteButton onClick={onDelete} />
+          </div>
+        )}
       </div>
-      <p className="text-sm">{description}</p>
+
+      {!isDeleted && (
+        <div
+          className={`overflow-hidden transition-all duration-500 ${visible ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+            }`}
+        >
+          <p className="text-sm">{description}</p>
+        </div>
+      )}
     </div>
   );
 };
